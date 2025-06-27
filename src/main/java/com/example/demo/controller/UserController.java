c04d09e9dd0617a4699a002b94097f9481ae3dff
Isuru <isuruindrajith680@gmail.com>
2025-06-27 14:22:31 +0530

    Controller package root

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
-    @GetMapping
+    @GetMapping("/getUser")
     public String getUser(){
         return "Spring boot";
     }
+
 }
