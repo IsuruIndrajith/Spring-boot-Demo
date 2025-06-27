616d110e92db0a0eee7c0d9fc20af634dd116e8c
Isuru <isuruindrajith680@gmail.com>
2025-06-27 14:40:41 +0530

    Request Mapping methods

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,17 +1,30 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
+
     @GetMapping("/getUser")
     public String getUser(){
         return "Spring boot";
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser(){
+        return "User updated.";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(){
+        return "User saved !";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(){
+        return "User Deleted!";
     }
 
 }
